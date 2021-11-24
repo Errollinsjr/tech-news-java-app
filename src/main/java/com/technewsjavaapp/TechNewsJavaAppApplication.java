ca5b21b643b70bac2a2ad53ca08731d2c47dd52f
Rollins, Edward <errollins.jr@gmail.com>
2021-11-24 08:18:45 -0500

    Initial commit

---
+++ src/main/java/com/technewsjavaapp/TechNewsJavaAppApplication.java
@@ -0,0 +1,13 @@
+package com.technewsjavaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechNewsJavaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechNewsJavaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/technewsjavaapp/TechNewsJavaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technewsjavaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechNewsJavaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
